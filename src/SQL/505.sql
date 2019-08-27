1040cf915a89ea8acd47aeb864a26dec9b3b3ccf
Kevin Barlett <kebarlet@microsoft.com>
2019-08-27 16:45:37 -0400

    CKL version check sproc

--- src/SQL/505.sql
+++ src/SQL/505.sql
@@ -35,7 +35,7 @@ DECLARE @UpdateVersion smallint
 DECLARE @CurrentVersion smallint
 DECLARE @VersionNotes varchar(MAX)
 SET @UpdateVersion = 505
-SET @VersionNotes = 'IIS Server/Site support | Additional referential integrity'
+SET @VersionNotes = 'IIS Server/Site support | Additional referential integrity | New object: sproc_CKLversionCheck'
 -- ===============================================================================================
 -- \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 -- ===============================================================================================
@@ -1379,6 +1379,73 @@ GO
 -- Ensure CheckListInfo and CheckListAttributes are hydrated 
 -- ==================================================================
 		EXEC PowerSTIG.sproc_ImportSTIGxml
+GO
+-- ==================================================================
+-- PowerSTIG.sproc_CKLversionCheck
+-- ==================================================================
+CREATE OR ALTER PROCEDURE PowerSTIG.sproc_CKLversionCheck
+		@Role varchar(256),
+		@CKLversion varchar(6),
+		@OS varchar(256)
+AS
+SET NOCOUNT ON
+--------------------------------------------------------------------------------- 
+-- The sample scripts are not supported under any Microsoft standard support 
+-- program or service. The sample scripts are provided AS IS without warranty  
+-- of any kind. Microsoft further disclaims all implied warranties including,  
+-- without limitation, any implied warranties of merchantability or of fitness for 
+-- a particular purpose. The entire risk arising out of the use or performance of  
+-- the sample scripts and documentation remains with you. In no event shall 
+-- Microsoft, its authors, or anyone else involved in the creation, production, or 
+-- delivery of the scripts be liable for any damages whatsoever (including, 
+-- without limitation, damages for loss of business profits, business interruption, 
+-- loss of business information, or other pecuniary loss) arising out of the use 
+-- of or inability to use the sample scripts or documentation, even if Microsoft 
+-- has been advised of the possibility of such damages 
+---------------------------------------------------------------------------------
+-- ===============================================================================================
+-- Purpose:
+-- Revisions:
+-- 08272019 - Kevin Barlett,Microsoft - Initial version.
+--Use example:
+--EXEC PowerSTIG.sproc_CKLversionCheck @ROLE = 'Excel2016',@cklversion='1.2',@OS='2012R2'
+--EXEC PowerSTIG.sproc_CKLversionCheck @ROLE = 'Excel2016',@cklversion='1.2',@OS='ALL'
+--EXEC PowerSTIG.sproc_CKLversionCheck @ROLE = 'WindowsServer-MS',@cklversion='1.7',@OS='2012R2'
+--EXEC PowerSTIG.sproc_CKLversionCheck @ROLE = 'WindowsServer-MS',@cklversion='1.7',@OS='2016'
+-- ===============================================================================================
+		SELECT
+			CASE
+				WHEN 
+					(
+						SELECT 1 AS CKLexists
+							FROM
+								PowerSTIG.CheckListInfo I
+							JOIN
+								PowerSTIG.ComplianceTypesInfo T
+							ON
+								I.RoleAlias = T.RoleAlias
+							JOIN
+								PowerSTIG.ComplianceTypes Y
+							ON
+								Y.ComplianceTypeID = T.ComplianceTypeID
+							JOIN
+								PowerSTIG.TargetTypeOS O
+							ON
+								O.OSid = T.OSid
+							WHERE
+								Y.ComplianceType = @Role
+								AND
+								I.ReleaseVersion = @CKLversion
+								AND
+								O.OSname = @OS
+					) = 1 
+				THEN
+					(SELECT 1 AS CKLexists)
+				ELSE
+					(SELECT 0 AS CKLexists)
+			END AS 
+				CKLexists
+GO
 -- ===============================================================================================
 -- ///////////////////////////////////////////////////////////////////////////////////////////////
 -- Logging
